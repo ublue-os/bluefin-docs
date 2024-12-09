4fd6eca4e455576bda137a5800c6d959a7ea9c8f
Jorge O. Castro <jorge.castro@gmail.com>
2024-12-09 16:25:29 -0500

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from "@docusaurus/preset-classic";
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: "Bluefin Docs",
+  title: "Bluefin",
   tagline: "Bluefin Documentation Resources",
   favicon: "img/favicon.ico",
 
